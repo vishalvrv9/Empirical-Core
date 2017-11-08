5260a4c76422fd27f61f321c5cb1e7cb0fab8e2e
RyanNovas <ryan@quill.org>
2017-11-08 12:29:11 -0500

    classrooms_teacher_spec

--- spec/controllers/teachers/progress_reports/concepts/students_controller_spec.rb
+++ spec/controllers/teachers/progress_reports/concepts/students_controller_spec.rb
@@ -10,11 +10,9 @@ describe Teachers::ProgressReports::Concepts::StudentsController, type: :control
       it 'includes a list of students in the JSON' do
         classrooms_arr = [{object: classroom_with_a_couple_students, class_size: 2}, {object: classroom_with_one_student, class_size: 1}, {object: classroom_with_students_and_activities, class_size: 5}]
         classrooms_arr.each do |classroom|
-          binding.pry
           session[:user_id] = classroom[:object].id
           get :index, format: :json
           json = JSON.parse(response.body)
-          binding.pry
         end
 
 
--- spec/models/classrooms_teacher_spec.rb
+++ spec/models/classrooms_teacher_spec.rb
@@ -1,10 +1,12 @@
 require 'rails_helper'
 
-RSpec.describe ClassroomsTeacher, type: :model do
+RSpec.describe ClassroomsTeacher, type: :model, redis: :true do
+  let(:classrooms_teacher_with_arbitrary_role) { build(:classrooms_teacher, role: 'hippopotamus') }
+  let(:classrooms_teacher_with_null_user_id) { build(:classrooms_teacher, user_id: nil) }
+  let(:classrooms_teacher_with_null_classroom_id) { build(:classrooms_teacher, classroom_id: nil) }
+  let(:classrooms_teacher) { build(:classrooms_teacher) }
+
   describe 'validations' do
-    let(:classrooms_teacher_with_arbitrary_role) { build(:classrooms_teacher, role: 'hippopotamus') }
-    let(:classrooms_teacher_with_null_user_id) { build(:classrooms_teacher, user_id: nil) }
-    let(:classrooms_teacher_with_null_classroom_id) { build(:classrooms_teacher, classroom_id: nil) }
 
     it 'should prevent saving arbitrary role' do
       expect{classrooms_teacher_with_arbitrary_role.save}.to raise_error ActiveRecord::StatementInvalid
@@ -19,15 +21,25 @@ RSpec.describe ClassroomsTeacher, type: :model do
     end
   end
 
-  # describe 'callbacks' do
-  #   it 'should trigger_analytics_events_for_classroom_creation on create commit' do
-  #
-  #   end
-  #
-  #   it 'should delete_classroom_minis_cache on create' do
-  #
-  #   end
-  # end
+  describe 'callbacks' do
+    let(:classroom) { create(:classroom, :with_no_teacher) }
+    let(:teacher) { create(:teacher) }
+    let(:classrooms_teacher) { build(:classrooms_teacher,
+      classroom_id: classroom.id,
+      user_id: teacher.id
+    )}
+    # it 'should trigger_analytics_events_for_classroom_creation on create commit' do
+    #   current_jobs = ClassroomCreationWorker.jobs.size
+    #   classrooms_teacher.save!
+    #   expect(ClassroomCreationWorker.jobs.size).to eq(current_jobs + 1)
+    # end
+
+    it 'should delete_classroom_minis_cache on create' do
+      $redis.set("user_id:#{teacher.id}_classroom_minis", {something: 'something'})
+      classrooms_teacher.save
+      expect($redis.get("user_id:#{teacher.id}_classroom_minis")).to eq nil
+    end
+  end
 
   describe 'associations' do
     let(:classroom) { create(:classroom, :with_no_teacher) }
